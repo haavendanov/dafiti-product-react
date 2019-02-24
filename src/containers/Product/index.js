e6af00bb628452e5a69cf96affeb10c13c832974
helmer.avendano@mercadoni.com <helmer.avendano@mercadoni.com>
2019-02-23 20:56:55 -0500

    Cambios estilos e instrucciones

--- src/containers/Product/index.js
+++ src/containers/Product/index.js
@@ -5,6 +5,10 @@ import ProductDetail from '../../components/Product/ProductDetail';
 import './styles.scss';
 
 class Product extends Component {
+  state = {
+    favorite: false,
+  }; 
+
   convertUnicode = input => {
     return input.replace(/\\u(\w\w\w\w)/g,function(a,b) {
       var charcode = parseInt(b,16);
@@ -12,14 +16,25 @@ class Product extends Component {
     });
   }
 
+  handleFavoriteClick = () => {
+    const { favorite } = this.state;
+    this.setState({favorite: !favorite})
+  }
+
   render() {
     const { product } = this.props;
+    const { favorite } = this.state;
     return (
       <div className="product">
         <img src={product.productimage} alt="product"/>
-        <span className="product__brand">{product.brand}</span>
-        <span className="product__name">{product.productname}</span>
-        <span className="product__seller">Vendido y entregado: <a href="#">{product.brand}</a></span>
+        <div className="product__information">
+          <div>
+            <span className="product__information__brand">{product.brand}</span>
+            <span className="product__information__name">{product.productname}</span>
+            <span className="product__information__seller">Vendido y entregado: <a href="#">{product.brand}</a></span>
+          </div>
+          { favorite ? <i className="fas fa-heart" onClick={this.handleFavoriteClick}></i> : <i className="far fa-heart" onClick={this.handleFavoriteClick}></i>}
+        </div>
         <ProductSizes sizes={product.sizes}/>
         <div className="product__share-buttons">
           <i className="fab fa-facebook-f"></i>
